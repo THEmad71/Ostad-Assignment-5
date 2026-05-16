a254b64469b6520e8812f9634a6e377084f8ae1d
Amad Uddin Osama <mrbangladesh19521971@gmail.com>
2026-05-17 02:44:54 +0600

    update backend url

--- news-portal/frontend/src/api/axios.js
+++ news-portal/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://news-portal-backend-lgph.onrender.com/api",
 });
 
 // প্রতিটা request এ automatically token যুক্ত করবে
